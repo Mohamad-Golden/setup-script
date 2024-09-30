7daad17f9b301b50c197bebc9f6ac41e3db0dc00
Mohamad <92015851+Mohamad-Golden@users.noreply.github.com>
2024-09-30 23:02:33 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,8 +17,7 @@ systemctl start xrdp
 
 echo "Installing software-properties-common..."
 apt install -y software-properties-common
-echo "Adding the deadsnakes PPA..."
-add-apt-repository ppa:deadsnakes/ppa
+add-apt-repository -y ppa:deadsnakes/ppa
 
 # Update package list again after adding PPA
 echo "Updating package list again..."
@@ -29,27 +28,27 @@ echo "Installing Python 3.10..."
 apt install -y python3.10 python3.10-venv python3.10-distutils
 
 echo "Installing vscode"
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudp apt install -y code
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
+add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+apt install -y code
 
 echo "Installing chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
 
 echo "Installing edge"
 wget https://packages.microsoft.com/keys/microsoft.asc -O microsoft.asc
 apt-key add microsoft.asc
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" | tee /etc/apt/sources.list.d/microsoft-edge.list
 apt update
-apt install microsoft-edge-stable
+apt install -y microsoft-edge-stable
 
 echo "Making edge default browser"
 xdg-settings set default-web-browser microsoft-edge.desktop
 
 echo "Creating user"
 useradd -m -s /bin/bash "$USERNAME"
-echo "$USERNAME:$PASSWORD" | sudo chpasswd
+echo "$USERNAME:$PASSWORD" | chpasswd
 
 
 echo "Creating project setup file"
@@ -73,5 +72,6 @@ EOL
 
 chmod +x "/home/$USERNAME/script.sh"
 
+
 echo "Installation complete. Rebooting the server..."
-sudo reboot
+reboot
